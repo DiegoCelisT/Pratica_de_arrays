dcc421f4bc03f487f517adbe6af513a6c663d3d5
Diego Celis <diegofercelis@gmail.com>
2021-02-19 21:15:15 -0300

    Inventando um exercicio com arrays, criarei um array multidimensional com as comidas do dia

---
+++ script.js
@@ -0,0 +1,5 @@
+var frutas = ["maçã", 'laranja', 'pera'];
+var bebidas = ['leite', 'café', 'choco'];
+var acompanhamentos = ['pão', 'cereal', 'aveia', 'ovos'];
+var café_da_manhã = [frutas, bebidas, acompanhamentos];
+console.log = café_da_manhã;
